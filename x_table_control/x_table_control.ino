6c29a3b490e0f8e142074e38bf4d6fe0fdf0016a
Nat Dempkowski <natdempk@gmail.com>
2017-12-28 15:19:53 -0500

    Rename speed related stuff to pulse widths, cleanup

--- x_table_control/x_table_control.ino
+++ x_table_control/x_table_control.ino
@@ -1,53 +1,47 @@
 //  MS1   MS2   MS3   Microstep Resolution
+//  --------------------------------------
 //  LOW   LOW   LOW   Full Step
 //  HIGH  LOW   LOW   Half Step
 //  LOW   HIGH  LOW   Quarter Step 
 //  HIGH  HIGH  LOW   Eighth Step
 //  HIGH  HIGH  HIGH  Sixteenth Step
 
-
-
-const int X_CONTROLLER_PIN = A0; //Analog x reading
+const int X_CONTROLLER_PIN = A0; // Analog x reading
 int xPos = 0;
-const int X_DIR_PIN = 4; //Set HIGH for forward, LOW for reverse
-const int X_STEP_PIN = 3; //PULSE to set speed
-const int X_MS1 = 0; //
-const int X_MS2 = 1; //
-const int X_MS3 = 2; //
+const int X_DIR_PIN = 4; // Set HIGH for forward, LOW for reverse
+const int X_STEP_PIN = 3; // PULSE to set speed
+const int X_MS1 = 0;
+const int X_MS2 = 1;
+const int X_MS3 = 2;
 
-// TODO: change
-const int Y_CONTROLLER_PIN = A1; //Analog y reading
+const int Y_CONTROLLER_PIN = A1; // Analog y reading
 int yPos = 0;
-const int Y_DIR_PIN = 12; //Set HIGH for forward, LOW for reverse
-const int Y_STEP_PIN = 11; //PULSE to set speed
-const int Y_MS1 = 8; // 
-const int Y_MS2 = 9; //
-const int Y_MS3 = 10; //
+const int Y_DIR_PIN = 12; // Set HIGH for forward, LOW for reverse
+const int Y_STEP_PIN = 11; // PULSE to set speed
+const int Y_MS1 = 8; 
+const int Y_MS2 = 9;
+const int Y_MS3 = 10;
 
-const int DELAY_SPEED = 1000;
-const int TOP_SPEED = 400;
-const int RAMP_SPEED = 3;
-const int STOP_SPEED = 8;
-/*int speedSet;*/
-int xSpeed = 0;
-int controllerXSpeed = 0;
-int ySpeed = 0;
-int controllerYSpeed = 0;
-// int off;
-/*boolean xShouldMove = false;*/
-/*boolean ySouldMove = false;*/
-
+const int LONGEST_PULSE_WIDTH_US = 1000;
+const int SHORTEST_PULSE_WIDTH_US = 400;
+const int RAMP_DELTA = 3;
+int xPulseWidth = 0;
+int controllerXPulseWidth = 0;
+int yPulseWidth = 0;
+int controllerYPulseWidth = 0;
 
 void setup() {
-  /*Serial.begin(9600);*/
+  // Serial.begin(9600);
   pinMode(X_DIR_PIN, OUTPUT);
   pinMode(X_STEP_PIN, OUTPUT);
   pinMode(X_MS1, OUTPUT);
   pinMode(X_MS2, OUTPUT);
   pinMode(X_MS3, OUTPUT);
+
   digitalWrite(X_MS1, LOW);
   digitalWrite(X_MS2, LOW);
   digitalWrite(X_MS3, LOW);
+
   pinMode(Y_DIR_PIN, OUTPUT);
   pinMode(Y_STEP_PIN, OUTPUT);
   pinMode(Y_MS1, OUTPUT);
@@ -59,99 +53,58 @@ void setup() {
   digitalWrite(Y_MS3, LOW);
 }
 
-int calculateNewSpeed(int currentSpeed, int newSpeed) {
-  int off = abs(currentSpeed - newSpeed);
+int calculateNewPulseWidth(int currentWidth, int newWidth) {
+  int offset = newWidth - currentWidth;
 
-  if (off !=  0 && currentSpeed < newSpeed) {
-    return currentSpeed + RAMP_SPEED;
-    /*Speed = Speed + RAMP_SPEED;*/
-  } else if (off !=  0 && currentSpeed > newSpeed) {
-    return currentSpeed - RAMP_SPEED;
-    /*Speed = Speed - RAMP_SPEED;*/
+  if (offset > 0) {
+    return currentWidth + RAMP_DELTA;
+  } else if (offset < 0) {
+    return currentWidth - RAMP_DELTA;
+  } else {
+    return currentWidth;
   }
 }
 
-void motorStep(int pin, int pinSpeed) {
+void sendMotorPulse(int pin, int pulseWidth) {
   digitalWrite(pin, HIGH);
-  delayMicroseconds(pinSpeed);
+  delayMicroseconds(pulseWidth);
   digitalWrite(pin, LOW);
-  delayMicroseconds(pinSpeed);
+  delayMicroseconds(pulseWidth);
 }
 
 void loop() {
   xPos = analogRead(X_CONTROLLER_PIN) + 5;
   yPos = analogRead(Y_CONTROLLER_PIN) + 5;
- 
-  /*Serial.println(xPos);
-  Serial.println(yPos);*/
 
   if (xPos >= 525) {
     digitalWrite(X_DIR_PIN, HIGH);
-    controllerXSpeed = map(xPos, 550, 1024, DELAY_SPEED, TOP_SPEED);
-    xSpeed = calculateNewSpeed(xSpeed, controllerXSpeed);
-
-  /*
-  Serial.print("HIGH speedSet=");
-  Serial.println(speedSet);
-  */
-    } 
-    
-    else if (xPos <= 475) {
+    controllerXPulseWidth = map(xPos, 550, 1024, LONGEST_PULSE_WIDTH_US, SHORTEST_PULSE_WIDTH_US);
+    xPulseWidth = calculateNewPulseWidth(xPulseWidth, controllerXPulseWidth);
+  } else if (xPos <= 475) {
     digitalWrite(X_DIR_PIN, LOW);
-    controllerXSpeed = map(xPos, 500, 0, DELAY_SPEED, TOP_SPEED);
-
-  /*
-  Serial.print("LOW speedSet=");
-  Serial.println(speedSet);
-  */
-  
-    xSpeed = calculateNewSpeed(xSpeed, controllerXSpeed);
-    } 
-    
-  else {
-    xSpeed = 0;
+    controllerXPulseWidth = map(xPos, 500, 0, LONGEST_PULSE_WIDTH_US, SHORTEST_PULSE_WIDTH_US);
+    xPulseWidth = calculateNewPulseWidth(xPulseWidth, controllerXPulseWidth);
+  } else {
+    xPulseWidth = 0;
   }
-
 
   if (yPos >= 550) {
     digitalWrite(Y_DIR_PIN, HIGH);
-    /*shouldMove = true;*/
-    controllerYSpeed = map(yPos, 550, 1024, DELAY_SPEED, TOP_SPEED);
-    /*Serial.print("HIGH controllerYSpeed=");*/
-    /*Serial.println(controllerYSpeed);*/
-    ySpeed = calculateNewSpeed(ySpeed, controllerYSpeed);
+    controllerYPulseWidth = map(yPos, 550, 1024, LONGEST_PULSE_WIDTH_US, SHORTEST_PULSE_WIDTH_US);
+    yPulseWidth = calculateNewPulseWidth(yPulseWidth, controllerYPulseWidth);
   } else if (yPos <= 475) {
     digitalWrite(Y_DIR_PIN, LOW);
-    /*shouldMove = true;*/
-    controllerYSpeed = map(yPos, 500, 0, DELAY_SPEED, TOP_SPEED);
-    /*Serial.print("LOW controllerYSpeed=");*/
-    /*Serial.println(controllerYSpeed);*/
-    ySpeed = calculateNewSpeed(ySpeed, controllerYSpeed);
+    controllerYPulseWidth = map(yPos, 500, 0, LONGEST_PULSE_WIDTH_US, SHORTEST_PULSE_WIDTH_US);
+    yPulseWidth = calculateNewPulseWidth(yPulseWidth, controllerYPulseWidth);
   } else {
-    /*shouldMove = false;*/
-    ySpeed = 0;
+    yPulseWidth = 0;
   }
 
-  /*Serial.print("ySpeed=");*/
-  /*Serial.println(ySpeed);*/
-
-  // bool shouldMove = ((ySpeed > 0) || (xSpeed > 0));
-
-  if (xSpeed > 0) {
-    motorStep(X_STEP_PIN, xSpeed);
+  if (xPulseWidth > 0) {
+    sendMotorPulse(X_STEP_PIN, xPulseWidth);
   }
-  if (ySpeed > 0) {
-    motorStep(Y_STEP_PIN, ySpeed);
+  if (yPulseWidth > 0) {
+    sendMotorPulse(Y_STEP_PIN, yPulseWidth);
   }
-
-  // ySpeed = 0;
-  // xSpeed = 0;
-    /*if (speed < delay_speed) {*/
-      /*speed = speed + stop_speed;*/
-      /*motorstep(steppin);*/
-    /*} else if (speed >= delay_speed) {*/
-      /*speed = delay_speed;*/
-    /*}*/
-  //}
 }
 
